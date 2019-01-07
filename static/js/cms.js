11484c992683fc1e0d8193e304eeeaf187698a8f
franckvalot <franck.valot@opencellsoft.com>
2019-01-07 14:32:35 +0700

    Update CMS preview

---
+++ static/js/cms.js
@@ -0,0 +1,12 @@
+var paradigmPreview = createClass({
+  render: function(){
+    var entry = this.props.entry;
+    var video = entry.getIn(['data', 'video']);
+    return h('div', {},
+      h('h1', {}, entry.getIn(['data', 'title'])),
+        h('img', {src: "test"}),
+          h('div', {"className": "text"}, this.props.widgetFor('body'))
+    )
+  }
+
+})
