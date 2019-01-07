3d77b22a674cdbd9f2075c6c0b88ebc2739f0349
franckvalot <franck.valot@opencellsoft.com>
2019-01-07 16:14:38 +0700

    Update CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -4,8 +4,7 @@ var ParadigmPreview = createClass({
     var videoId = entry.getIn(['data', 'video']);
     return h('div', {className:"row justify-content-center"},
       h('div', {className:"col-sm-12 col-md-10 col-lg-8 embed-responsive embed-responsive-16by9"},
-        h('iframe', {className:"youtube", src:'https://www.youtube.com/embed/'+ videoId +'?controls=0', frameborder:'0', allow:'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture', allowfullscreen}, )
-      )
+        "<iframe class=\"youtube\" src=\"https://www.youtube.com/embed/"+ videoId + "?controls=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     )
   }
 })
