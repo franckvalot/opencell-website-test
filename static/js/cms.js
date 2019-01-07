5e26d9ade83a975d5fe25aaf74bcfbd162203bc3
franckvalot <franck.valot@opencellsoft.com>
2019-01-07 15:24:09 +0700

    Update CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -1,12 +1,7 @@
 var ParadigmPreview = createClass({
   render: function(){
     var entry = this.props.entry;
-    var video = entry.getIn(['data', 'video']);
-    return h('div', {},
-      h('h1', {}, entry.getIn(['data', 'title'])),
-        h('img', {src: "test"}),
-          h('div', {"className": "text"}, this.props.widgetFor('body'))
-    )
+    var videoId = entry.getIn(['data', 'video']);
+    return h('div', {}, videoId)
   }
-
 })
