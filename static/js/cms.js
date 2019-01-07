a58343c4e0bfd225f3405f03564d03d07a978a02
franckvalot <franck.valot@opencellsoft.com>
2019-01-07 16:40:39 +0700

    Update CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -11,5 +11,28 @@ var ParadigmPreview = createClass({
 })
 
 
+var WhatWeProvideView = createClass({
+  render: function(){
+    var entry = this.props.entry;
+    var content = entry.getIn(['data', 'content']);
+    var image = entry.getIn(['data', 'image']);
+    return h('div', {className:'container'},
+      h('div', {className:'row align-items-center justify-content-center'},
+        h('h1', {className:'col-12 text-center'}, 'What we provide.')
+      ),
+      h('div', {className:'row align-items-center justify-content-center'},
+        h('div', {className:'col-12 col-md-8 col-lg-6'},
+          h('img', {src:image, className:'img-fluid'},'')
+        ),
+        h('div', {className:'col-12 col-md-4 col-lg-6'},
+          content,
+          h('button', {type:'button', name:'button', className:'opencell-btn'}, 'DISCOVER')
+        )
+      )
+    )
+  }
+})
+
 CMS.registerPreviewTemplate("paradigm", ParadigmPreview);
+CMS.registerPreviewTemplate("whatweprovide", WhatWeProvideView);
 CMS.registerPreviewStyle("/css/cms.css");
