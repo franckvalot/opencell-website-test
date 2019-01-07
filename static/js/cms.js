a208cc85ae4d164460b7770f939af5adc08ae25a
franckvalot <franck.valot@opencellsoft.com>
2019-01-07 16:17:39 +0700

    Fix CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -5,6 +5,7 @@ var ParadigmPreview = createClass({
     return h('div', {className:"row justify-content-center"},
       h('div', {className:"col-sm-12 col-md-10 col-lg-8 embed-responsive embed-responsive-16by9"},
         "<iframe class=\"youtube\" src=\"https://www.youtube.com/embed/"+ videoId + "?controls=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+      )
     )
   }
 })
