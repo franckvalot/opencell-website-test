63ccaac6621dd9ee5460cdd2993391c56be552c3
franckvalot <franck.valot@opencellsoft.com>
2019-01-07 17:48:28 +0700

    Update CMS preview Update CMS config Update Index

--- static/js/cms.js
+++ static/js/cms.js
@@ -18,7 +18,7 @@ var WhatWeProvideView = createClass({
     var image = entry.getIn(['data', 'image']);
     return h('div', {className:'container'},
       h('div', {className:'row align-items-center justify-content-center'},
-        h('h1', {className:'col-12 text-center'}, 'What we provide.')
+        h('h1', {className:'col text-center'}, 'What we provide.')
       ),
       h('div', {className:'row align-items-center justify-content-center'},
         h('div', {className:'col-12 col-md-8 col-lg-6'},
@@ -32,6 +32,54 @@ var WhatWeProvideView = createClass({
     )
   }
 })
+
+var WhatMakesUsView = createClass({
+  render: function(){
+    var entry = this.props.entry;
+
+    return h('div', {className:'container'},
+      h('div', {className:'row justify-content-center'},
+        h('h1'), {className:'col text-center', 'What makes us'}
+      ),
+      h('div', {className:'row justify-content-center whatmakesus text-center'},
+        h('div', {className:'col-8 col-md-4'},
+          h('div', {className:'title-box text-center'}, entry.getIn(['data', 'title1'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_1_1'])),
+          h('p', {}, entry.getIn(['data', 'text_1_1'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_1_2'])),
+          h('p', {}, entry.getIn(['data', 'text_1_2'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_1_3'])),
+          h('p', {}, entry.getIn(['data', 'text-1_3'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_1_4'])),
+          h('p', {}, entry.getIn(['data', 'text_1_4'])),
+        ),
+        h('div', {className:'col-8 col-md-4'},
+          h('div', {className:'title-box text-center'}, entry.getIn(['data', 'title2'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_2_1'])),
+          h('p', {}, entry.getIn(['data', 'text_2_1'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_2_2'])),
+          h('p', {}, entry.getIn(['data', 'text_2_2'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_2_3'])),
+          h('p', {}, entry.getIn(['data', 'text_2_3'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_2_4'])),
+          h('p', {}, entry.getIn(['data', 'text_2_4'])),
+        ),
+        h('div', {className:'col-8 col-md-4'},
+          h('div', {className:'title-box text-center'}, entry.getIn(['data', 'title3'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_3_1'])),
+          h('p', {}, entry.getIn(['data', 'text_3_1'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_3_2'])),
+          h('p', {}, entry.getIn(['data', 'text_3_2'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_3_3'])),
+          h('p', {}, entry.getIn(['data', 'text_3_3'])),
+          h('h2', {}, entry.getIn(['data', 'subtitle_3_4'])),
+          h('p', {}, entry.getIn(['data', 'text_3_4']))
+      )
+      )
+    )
+  }
+})
+
 
 CMS.registerPreviewTemplate("paradigm", ParadigmPreview);
 CMS.registerPreviewTemplate("whatweprovide", WhatWeProvideView);
