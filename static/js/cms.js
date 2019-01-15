fb042e7455406f75100b7cd4e24f2ef2668ab991
franckvalot <franck.valot@opencellsoft.com>
2019-01-15 12:37:42 +0700

    Fix CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -21,6 +21,18 @@ var HeaderPreview = createClass({
       else return h('li', {'data-target':'"#carouselHeader', 'data-slide-to':index}, '');
     }
 
+    var carouselInner = function(item, index){
+      if(index==0) return
+      h('div', {className:'carousel-item active'},
+        h('h1', {}, item.title)),
+        h('p', {}, item.subtitle))
+      );
+      else return
+      h('div', {className:'carousel-item'},
+        h('h1', {}, item.title)),
+        h('p', {}, item.subtitle))
+      );
+    }
 
     return h('header', {className:'hero'},
       h('div', {className:'header-content-inner'},
@@ -29,18 +41,7 @@ var HeaderPreview = createClass({
             carousel.map(carouselIndicator)
           ),
           h('div', {className:'carousel-inner'},
-            h('div', {className:'carousel-item active'},
-              h('h1', {}, entry.getIn(['data', 'title1'])),
-              h('p', {}, entry.getIn(['data', 'subtitle1']))
-            ),
-            h('div', {className:'carousel-item'},
-              h('h1', {}, entry.getIn(['data', 'title2'])),
-              h('p', {}, entry.getIn(['data', 'subtitle2']))
-            ),
-            h('div', {className:'carousel-item'},
-              h('h1', {}, entry.getIn(['data', 'title3'])),
-              h('p', {}, entry.getIn(['data', 'subtitle3']))
-            )
+            carousel.map(carouselInner);
           ),
           h('a', {className:'carousel-control-prev', href:'#carouselHeader', role:'button', 'data-slide':'prev'},
             h('span', {className:'carousel-control-prev-icon', 'area-hidden':'true'}, ''),
