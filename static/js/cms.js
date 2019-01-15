d9d8a9c66fba1078e37fe16114aefd68133bb3e2
franckvalot <franck.valot@opencellsoft.com>
2019-01-15 12:58:21 +0700

    Fix CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -21,8 +21,16 @@ var HeaderPreview = createClass({
       else return h('li', {'data-target':'"#carouselHeader', 'data-slide-to':index}, '');
     }
     var carouselInner = function(item, index){
-      if(index==0) return h('div', {className:'carousel-item active'}, "Active" + index );
-      else return h('div', {className:'carousel-item'}, "Innactive" + index );
+      if(index==0) return
+        h('div', {className:'carousel-item active'},
+          h('h1', {}, item.title),
+          h('p', {}, item.subtitle)
+        );
+      else return
+        h('div', {className:'carousel-item'},
+          h('h1', {}, item.title),
+          h('p', {}, item.subtitle)
+        );
     }
 
     return h('header', {className:'hero'},
