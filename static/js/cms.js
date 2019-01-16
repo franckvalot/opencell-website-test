0179c0c8981f6ff65ed8ecea0d5b4b5fca5fb96c
franckvalot <franck.valot@opencellsoft.com>
2019-01-16 19:38:42 +0700

    Test CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -428,8 +428,8 @@ function smallHeader(item){
 function titleanddescription(item){
   return h('div', {className:'row justify-content-center'},
       h('h1', {className:'col-12 text-center'}, item.get('title'))
-    ),
-    h('div', {className:'row justify-content-center text-center'},
+    )
+  + h('div', {className:'row justify-content-center text-center'},
       h('div', {className:'col-10 col-md-8'}, item.get('content'))
     );
 }
