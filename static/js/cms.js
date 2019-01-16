f7533aa8504c189fac523a6bc35311558e9a3024
franckvalot <franck.valot@opencellsoft.com>
2019-01-16 19:30:07 +0700

    Test CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -426,14 +426,13 @@ function smallHeader(item){
 }
 
 function titleanddescription(item){
-  return h('div', {},
+  return
     h('div', {className:'row justify-content-center'},
       h('h1', {className:'col-12 text-center'}, item.get('title'))
     ),
     h('div', {className:'row justify-content-center text-center'},
       h('div', {className:'col-10 col-md-8'}, item.get('content'))
-    )
-  );
+    );
 }
 
 var AboutUSStoryPreview = createClass({
