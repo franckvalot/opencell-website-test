a83f6c7320f06c94b9c55906995f19871eae217f
franckvalot <franck.valot@opencellsoft.com>
2019-01-17 15:03:38 +0700

    Fix CMS preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -431,11 +431,7 @@ function titleanddescription(item){
     ),
     h('div', {className:'row justify-content-center text-center'},
       h('div', {className:'col-10 col-md-8'}, item.get('content'))
-    ),
-    if(item.get('img') != null ) [h('div', {className:'w-100'},''),
-    h('div', {className:'col'},
-      h('img', {className:'img-fluid', src: item.get('img').get('url'), alt:item.get('img').get('alt')})
-    )]
+    )
   ];
 }
 
