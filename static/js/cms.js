c92e908a91b025ea3b0bf377dfce26b11581b167
franckvalot <franck.valot@opencellsoft.com>
2019-01-29 15:10:59 +0700

    Fix CMS preview - innerhtml

--- static/js/cms.js
+++ static/js/cms.js
@@ -66,6 +66,14 @@ function logos(item){
   );
 }
 
+function HTML(item){
+    return {__html: item};
+}
+function innerHTML(item){
+  return dangerouslySetInnerHTML={HTML(item);
+
+}
+
 var PlatformModulesPreview = createClass({
   render: function(){
       var entry = this.props.entry;
@@ -158,7 +166,7 @@ var PlatformTechnologyPreview = createClass({
       var columns = function(item){
         return h('div', {className:'col-12 col-md-4'},
           h('div', {className:'row justify-content-center'},
-            h('div', {className:'title-box text-center'},dangerouslySetInnerHTML:{_html:item.get('title')} ),
+            h('div', {className:'title-box text-center'}, innerHTML(item.get('title')) ),
             h('div', {className:'col-12'},
               item.get('content').map(content)
             )
