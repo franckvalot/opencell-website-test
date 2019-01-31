28b87f877c4ea4c44f6995f4f1d087b33040b5ce
franckvalot <franck.valot@opencellsoft.com>
2019-01-31 15:39:47 +0700

    Fix CMS preview - whatweprovide

--- static/js/cms.js
+++ static/js/cms.js
@@ -124,7 +124,7 @@ var IndexPreview = createClass({
               h('img', {className:'img-fluid', src: whatweprovidedata.get('image'), alt:''})
             ),
             h('div', {className:'col-12 col-md-4 col-lg-6'},
-              whatweprovide.widgetFor('content'),
+              whatweprovidedata.widgetFor('content'),
               h('button', {className:'opencell-btn'}, 'DISCOVER')
             )
           )
